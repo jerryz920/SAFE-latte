1f72147995c455b41afed2a3e0df534732fe8061
qiang <qiang.cao@duke.edu>
2018-10-03 23:41:07 -0400

    updated safe dockefile and re-orged strong slang scripst

--- dockerfiles/safe/start.sh
+++ dockerfiles/safe/start.sh
@@ -6,6 +6,10 @@ cd ~/SAFE
 # Set up: configuration the storage server for SAFE
 sed -i "/.*url = \"http/ s:.*:    url = \"http\://${RIAK_IP}\:8098/types/safesets/buckets/safe/keys\":" safe-server/src/main/resources/application.conf
 
+strong_root=`python hash_gen.py /principalkeys/${STRONG_ROOT_PUB}`
+
+sed -i "/.*defenv RootDir()/ s:.*:defenv RootDir() \:- \"${strong_root}\:root\":" /imports/${SLANG_CONF}
+
 # Run
 ~/sbt/bin/sbt "project safe-server" "run -f /imports/${SLANG_SCRIPT} -r safeService  -kd  /principalkeys"
 
