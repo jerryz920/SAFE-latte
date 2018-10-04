e534dfec66c685c2973199503bc9f5abc83d9b20
qiang <qiang.cao@duke.edu>
2018-10-04 13:10:08 -0400

    updated safe dockerfile and start script

--- dockerfiles/safe/start.sh
+++ dockerfiles/safe/start.sh
@@ -6,7 +6,7 @@ cd ~/SAFE
 # Set up: configuration the storage server for SAFE
 sed -i "/.*url = \"http/ s:.*:    url = \"http\://${RIAK_IP}\:8098/types/safesets/buckets/safe/keys\":" safe-server/src/main/resources/application.conf
 
-strong_root=`python hash_gen.py /principalkeys/${STRONG_ROOT_PUB}`
+strong_root=`python /root/hash_gen.py /principalkeys/${STRONG_ROOT_PUB}`
 
 sed -i "/.*defenv RootDir()/ s:.*:defenv RootDir() \:- \"${strong_root}\:root\":" /imports/${SLANG_CONF}
 
