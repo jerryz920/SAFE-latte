37f941e0e597d5b50986151768230e6ec41ebe04
qiang <qiang.cao@duke.edu>
2018-10-20 19:11:37 -0400

    some cleaning for slang shell code

--- safe-logic/src/main/scala/safe/safelog/Repl.scala
+++ safe-logic/src/main/scala/safe/safelog/Repl.scala
@@ -30,7 +30,7 @@ class Repl(
   private val _retractionsInMemory: MutableSet[Retraction] = MutableSet.empty     // Parsed retractions
   private val _queriesInMemoryFresh: MutableSet[Statement] = MutableSet.empty     // Parsed queries which are fresh, i.e., not executed yet
   private var _inputScanned: StringBuilder = new StringBuilder()  // Stores incomplete statements that end with other than (.|?|~)
-  private val _replStatements = new MutableCache[Index, OrderedSet[Statement]]()  // All statements, for inference 
+  private val _replStatements = new MutableCache[Index, OrderedSet[Statement]]()  // All statements, for inference
 
   private val replCmdSeq = Seq(
       "clear."
@@ -75,10 +75,10 @@ class Repl(
     import jline.console.history.FileHistory
 
     fileName match {
-      case Some(fn) => // console reader backed by a specified history file 
+      case Some(fn) => // console reader backed by a specified history file
         val file = Term.stripQuotes(fn)
         try {
-          val fStream = this.getClass().getClassLoader().getResourceAsStream(file) match { // for externalized resources 
+          val fStream = this.getClass().getClassLoader().getResourceAsStream(file) match { // for externalized resources
             case null => this.getClass().getClassLoader().getResourceAsStream(file + ".slog")
             case x => x
           }
@@ -94,7 +94,7 @@ class Repl(
          case e: NullPointerException => throw ParserException(s"Error reading file: $file")
          case _: Throwable => throw ParserException(s"Error reading file: $file")
        }
-      case _ => 
+      case _ =>
 	val reader = new jline.console.ConsoleReader(System.in, System.out)
         val historyFile: File = new File(System.getProperty("user.home") + "/.slang_history")
         if(historyFile.exists()) { // clear existing history if any
@@ -129,7 +129,7 @@ class Repl(
 
   private def updateEnvContext(k: StrLit, v: EnvValue): Unit = {
     envContext.put(k, v)
-    updatePromptSelf() 
+    updatePromptSelf()
   }
 
   private def clearEnvContext(): Unit = {
@@ -146,17 +146,17 @@ class Repl(
     var isReplBuiltin = true
 
     cmd match {
-      case Structure(StrLit("_is"), (v @ Variable(StrLit(vname), _, _, _)) +: rightterm +: Nil, _, _, _) => 
-        //println(s"Set ${vname} to ${rightterm}.  rightterm.getClass=${rightterm.getClass}")      
+      case Structure(StrLit("_is"), (v @ Variable(StrLit(vname), _, _, _)) +: rightterm +: Nil, _, _, _) =>
+        //println(s"Set ${vname} to ${rightterm}.  rightterm.getClass=${rightterm.getClass}")
         //if(rightterm.isInstanceOf[Structure]) {
         //  println(s"rightterm.id=${rightterm.asInstanceOf[Structure].id}")
         //  println(s"rightterm.terms=${rightterm.asInstanceOf[Structure].terms}")
         //}
-        //val rightval = solveAQuery(Query(Seq(cmd)))(envContext, Seq[Subcontext]()).map(f => f(v)) 
+        //val rightval = solveAQuery(Query(Seq(cmd)))(envContext, Seq[Subcontext]()).map(f => f(v))
         try {
-          val rightval = solveAQuery(Query(Seq(cmd)))(envContext, Seq(Subcontext("_tmp", _replStatements.toMap))).map(f => f(v)) 
+          val rightval = solveAQuery(Query(Seq(cmd)))(envContext, Seq(Subcontext("_tmp", _replStatements.toMap))).map(f => f(v))
           updateEnvContext(v.simpleName, rightval.head)
-          printLabel('info) 
+          printLabel('info)
           println(s"${v.simpleName.name}=${rightval.head}") //(${rightval})")
           //println(s"envContext=${envContext}")
         } catch {
@@ -165,17 +165,17 @@ class Repl(
              println(ex.toString)
         }
 
-      case Structure(StrLit("clear"), Seq(Constant(StrLit("env"), _, _, _)), _, _, _) => 
+      case Structure(StrLit("clear"), Seq(Constant(StrLit("env"), _, _, _)), _, _, _) =>
         clearEnvContext()  // clear env variables
 
-      case Constant(StrLit("clear"), _, _, _) | Structure(StrLit("clear"), Nil, _, _, _) => 
-        printLabel('info); println("clear all") 
+      case Constant(StrLit("clear"), _, _, _) | Structure(StrLit("clear"), Nil, _, _, _) =>
+        printLabel('info); println("clear all")
         _assertionsInMemory.clear()
         _queriesInMemory.clear()
         _replStatements.clear()
         clearEnvContext()
 
-      case Structure(StrLit("saveEnvTo"), Seq(Constant(StrLit(file), _, _, _)), _, _, _) => 
+      case Structure(StrLit("saveEnvTo"), Seq(Constant(StrLit(file), _, _, _)), _, _, _) =>
         try {
           val p = Repl.expandPathname(file)
           val pw = new PrintWriter(new File(p))
@@ -187,18 +187,18 @@ class Repl(
              println(ex.toString)
         }
 
-      case Structure(StrLit("runShellCmds"), Seq(Constant(StrLit(file), _, _, _)), _, _, _) => 
+      case Structure(StrLit("runShellCmds"), Seq(Constant(StrLit(file), _, _, _)), _, _, _) =>
         val p = Repl.expandPathname(file)
         val source = scala.io.Source.fromFile(p)
         val _inputScanned = source.getLines.mkString("\n")
           _inputScanned.toString match {
             case str => parseCmdLine(str) match {
-              case (Some(stmts), 'success) =>  // program includes all statement 
+              case (Some(stmts), 'success) =>  // program includes all statement
                 val program = addStatementsToRepl(stmts)     // add to _replStatements
                 // println(s"[Repl.evalReplCmd.runShellCmds] scanned stmts: $stmts")
                 processShellCommands(stmts.toMap, true)
               case (None, 'continuation) =>
-                println("Unexpected end of command list") 
+                println("Unexpected end of command list")
               case x @ _ => // no messages other than parsing error messages
             }
           }
@@ -206,43 +206,43 @@ class Repl(
       case Constant(StrLit("env"), _, _, _) | Structure(StrLit("env"), Nil, _, _, _) =>
         envContext.foreach{ case (k, v) if v.isInstanceOf[Constant] => println(s"${k.name}=${v.asInstanceOf[Constant].id.name}") }
 
-      case Constant(StrLit("pwd"), _, _, _) | Structure(StrLit("pwd"), Nil, _, _, _) => 
+      case Constant(StrLit("pwd"), _, _, _) | Structure(StrLit("pwd"), Nil, _, _, _) =>
         val workingDir: Path = Paths.get(".")
         println(workingDir.toFile.getCanonicalPath)
 
-      case Constant(StrLit("ls"), _, _, _) | Structure(StrLit("ls"), Nil, _, _, _) => 
+      case Constant(StrLit("ls"), _, _, _) | Structure(StrLit("ls"), Nil, _, _, _) =>
         //printLabel('info); println("list of assertions made so far ...")
         _assertionsInMemory.foreach(println)
 
-      case Structure(StrLit("_fact"), Variable(StrLit("$Self"), _, _, _) +: Constant(StrLit("ls"), _, _, _) +: Nil, _, _, _) 
-         | Structure(StrLit("ls"), Nil, _, _, _) => 
+      case Structure(StrLit("_fact"), Variable(StrLit("$Self"), _, _, _) +: Constant(StrLit("ls"), _, _, _) +: Nil, _, _, _)
+         | Structure(StrLit("ls"), Nil, _, _, _) =>
         printLabel('info); println("list of assertions made so far ...")
         _assertionsInMemory.foreach(println)
 
-      case Structure(StrLit("ls"), Seq(Constant(StrLit("all"), _, _, _)), _, _, _) => 
+      case Structure(StrLit("ls"), Seq(Constant(StrLit("all"), _, _, _)), _, _, _) =>
         //printLabel('info); println("list of all statements made so far...")
         _assertionsInMemory.foreach(println)
         _retractionsInMemory.foreach(println)
         _queriesInMemory.foreach(println)
 
-      case Structure(StrLit("ls"), Seq(Constant(StrLit("facts"), _, _, _)), _, _, _) => 
+      case Structure(StrLit("ls"), Seq(Constant(StrLit("facts"), _, _, _)), _, _, _) =>
         //printLabel('info); println("facts made so far ...")
         _assertionsInMemory.filter(_.terms.length < 2).foreach(println)
 
-      case Structure(StrLit("ls"), Seq(Constant(StrLit("rules"), _, _, _)), _, _, _) => 
+      case Structure(StrLit("ls"), Seq(Constant(StrLit("rules"), _, _, _)), _, _, _) =>
         printLabel('info); println("rules made so far ....")
         _assertionsInMemory.filter(_.terms.length > 1).foreach(println)
 
-      case Structure(StrLit("ls"), Seq(Constant(StrLit("queries"), _, _, _)), _, _, _) => 
+      case Structure(StrLit("ls"), Seq(Constant(StrLit("queries"), _, _, _)), _, _, _) =>
         printLabel('info); println("queries made so far ....")
         _queriesInMemory.foreach(println)
 
-      case Structure(StrLit("ls"), Seq(Constant(StrLit("retractions"), _, _, _)), _, _, _) => 
+      case Structure(StrLit("ls"), Seq(Constant(StrLit("retractions"), _, _, _)), _, _, _) =>
         printLabel('info); println("retractions made so far ....")
         _retractionsInMemory.foreach(println)
 
-      case Constant(StrLit("help"), _, _, _) | Structure(StrLit("help"), Nil, _, _, _) => 
-        printLabel('info); println("commands list") 
+      case Constant(StrLit("help"), _, _, _) | Structure(StrLit("help"), Nil, _, _, _) =>
+        printLabel('info); println("commands list")
         println("help.                     display this message")
         println("env.                      list of envs")
         println("saveEnvTo(<file>)         save envs to a file")
@@ -256,12 +256,12 @@ class Repl(
         println("ls(all).                  list of all statements made so far")
         println("q().                      quit the interpreter")
 
-      case Structure(StrLit("import"), xterms, _, _, _) => 
+      case Structure(StrLit("import"), xterms, _, _, _) =>
         val (filePathname, subjectSha) = xterms match {
            case Seq(Constant(filePathname, _, _, _)) => (filePathname, "")
            case Seq(Constant(filePathname, _, _, _), Constant(sub, _, _, _)) => (filePathname, sub)
         }
-        printLabel('info); println(s"importing file ${filePathname.name} ...") 
+        printLabel('info); println(s"importing file ${filePathname.name} ...")
         //println(s"cmd.primaryIndex=${cmd.primaryIndex} \n  cmd.secondaryIndex=${cmd.secondaryIndex}")
         //_replStatements.remove(cmd.primaryIndex) // remove this import from cache to avoid unneeded recursion
         //_replStatements.remove(cmd.secondaryIndex)
@@ -271,7 +271,7 @@ class Repl(
         var importedProgram = Map[Index, OrderedSet[Statement]]()
         try {
           val file = filePathname.name
-          //val source = io.Source.fromFile(file).getLines.toList.mkString("\n") 
+          //val source = io.Source.fromFile(file).getLines.toList.mkString("\n")
           //val fStream = new java.io.InputStreamReader(new java.io.FileInputStream(file))
           //val fStream = new java.io.InputStreamReader(this.getClass().getClassLoader().getResourceAsStream(file))
           //val (knowledgeBase, time) = util.time(parse(fStream))
@@ -282,15 +282,15 @@ class Repl(
           logger.info(s"Time for import is $time seconds")
           println("Imported in %f seconds".format(time))
         } catch {
-          case ex: Exception =>  
+          case ex: Exception =>
             println(s"Failed to parse ${filePathname.name}: ${ex}")
             return true
         }
         importedProgram.values().flatten.foreach {
           case s @ Assertion(stmt) => stmt.head match {
-             case Structure(StrLit("import"), _, _, _, _) => 
+             case Structure(StrLit("import"), _, _, _, _) =>
                // evalReplCmd(stmt.head)
-               println("[" + Console.RED + "Unexpected import" + Console.RESET + "] " + s) 
+               println("[" + Console.RED + "Unexpected import" + Console.RESET + "] " + s)
              case _ => _assertionsInMemory += s
           }
           case s @ Retraction(stmt) => _retractionsInMemory += s
@@ -302,14 +302,14 @@ class Repl(
       case Structure(StrLit("trace"), Seq(Constant(StrLit("on"), _, _, _)), _, _, _) => printLabel('info); println("starting the trace ...")
         //env._trace = true
 
-      case Structure(StrLit("trace"), Seq(Constant(StrLit("off"), _, _, _)), _, _, _) => printLabel('info); println("trace is off ...") 
+      case Structure(StrLit("trace"), Seq(Constant(StrLit("off"), _, _, _)), _, _, _) => printLabel('info); println("trace is off ...")
         //env._trace = false
 
       case _ => isReplBuiltin = false
     }
-   
+
     if(isReplBuiltin) { // already executed; remove the cmd
-      _replStatements.remove(cmd.primaryIndex) 
+      _replStatements.remove(cmd.primaryIndex)
       true
     } else {
       false
@@ -335,14 +335,14 @@ class Repl(
 
     _inputScanned.toString match {
       case str => parseCmdLine(str) match {
-	case (Some(stmts), 'success) =>  // program includes all statement 
+	case (Some(stmts), 'success) =>  // program includes all statement
           val program = addStatementsToRepl(stmts)     // add to _replStatements
           processShellCommands(stmts.toMap, interactiveMode)
-        case (None, 'quit) => 
+        case (None, 'quit) =>
           end = true
           if(interactiveMode) flushHistory(reader)
 	case (None, 'comment) =>  // a line starting with a comment
-	case (None, 'paste) => 
+	case (None, 'paste) =>
           stdPromptMsg = " | "
           uncompletedLine = true
 	case (None, 'continuation) => // empty line or incomplete statement
@@ -356,7 +356,7 @@ class Repl(
         case (_, 'error) =>
       }
       if(!uncompletedLine) {
-        _inputScanned.clear()  
+        _inputScanned.clear()
       }
       if(!end) {
         if(interactiveMode) readEval(reader, stdPromptMsg, true) else readEval(reader)
@@ -383,16 +383,16 @@ class Repl(
     try{
       commands.values().flatten.map {
          case assertion @ Assertion(stmt) =>
-           if(interactiveMode && !evalReplCmd(stmt.head)) { 
+           if(interactiveMode && !evalReplCmd(stmt.head)) {
              //println(s"readEval: done with ${stmt.head};  interactiveMode=${interactiveMode}")
              //scala.io.StdIn.readLine()
              _assertionsInMemory += assertion }
            if(!interactiveMode) _assertionsInMemory += assertion
            if(assertion.terms.head.id.name == "clear") throw new UnSafeException("clear")
-         case query @ Query(stmt) => 
+         case query @ Query(stmt) =>
            _replStatements.remove(StrLit("_query"))
            updateQuery(query, stmt)
-         case query @ QueryAll(stmt) => 
+         case query @ QueryAll(stmt) =>
            _replStatements.remove(StrLit("_query"))
            updateQuery(query, stmt)
          case _ => Nil
@@ -409,10 +409,10 @@ class Repl(
         val (solutions: Seq[Seq[Statement]], time: Double) = util.time(solve(programMap, _queriesInMemoryFresh.toSeq, interactiveMode), 'ms)
         logger.info(s"Solve completed in $time milliseconds")
         //println(s"Solve completed in $time milliseconds")
-        if(solutions.flatten.nonEmpty) true //printLabel('success) //println(s"""All solutions: ${solutions.mkString(", ")}""") 
+        if(solutions.flatten.nonEmpty) true //printLabel('success) //println(s"""All solutions: ${solutions.mkString(", ")}""")
         else false // printLabel('failure)
       } catch {
-        case ex: Throwable => 
+        case ex: Throwable =>
           printLabel('failure)
           println(ex.toString)
           //logger.error(ex.toString)
@@ -497,9 +497,9 @@ class Repl(
       logger.info(s"Solve completed in $time milliseconds")
       solutions
     } catch {
-      case ex: Throwable => 
+      case ex: Throwable =>
         logger.error(ex.toString)
-        Nil 
+        Nil
     }
   }
 
@@ -527,7 +527,7 @@ class Repl(
   }
 
   def importProgram(stmt: Statement, program: ConcurrentHashMap[Index, Set[Statement]]): ConcurrentHashMap[Index, Set[Statement]] = stmt.terms.head match {
-    case Structure('import, Seq(Constant(fileName))) => printLabel('info); println(s"importing program with name $fileName ...") 
+    case Structure('import, Seq(Constant(fileName))) => printLabel('info); println(s"importing program with name $fileName ...")
       val file = fileName.name
       val fStream = new java.io.InputStreamReader(this.getClass().getClassLoader().getResourceAsStream(file))
       val importedProgram = parse(fStream)
@@ -573,7 +573,7 @@ object Repl {
     } else if(argMap.get('file).getOrElse("nil") != "nil") {
       inference.printOutput(inference.evalFileWithTime(argMap.get('file).get, 'ms)) // Evaluate expressions from a file.
     } else {
-      inference.repl() // The init call 
+      inference.repl() // The init call
     }
     //throw new Exception("Incorrect arguments")
   }
