d3cff0288b9e16aab7309e657b0923d9be3f4b6c
qiang <qiang.cao@duke.edu>
2019-03-18 13:27:53 -0400

    refactoring testing code

--- safe-benchmark/src/main/scala/safe/benchmark/PrincipalStub.scala
+++ safe-benchmark/src/main/scala/safe/benchmark/PrincipalStub.scala
@@ -14,36 +14,40 @@ class PrincipalStub (
     protected val pid: String,                     // Principal id: hash of public key
     protected val canonicalName: String,           // Symbolic name of the principal
     protected val serverJvm: String,               // Address of the principal's JVM
-    protected var subjectSetTokens: Seq[String],   // Tokens of his subject sets 
-    protected val keyFile: String                  // pem filepath
+    protected var credSetTokens: Seq[String],      // Tokens of credential sets, including subject set 
+    protected val keyFile: String                  // Key file pathname
   ) extends Serializable with SlangObjectHelper {
- 
-  def this(publicKey: PublicKey, cn: String, serverJvm: String, subjectSetTokens: Seq[String]) {
-    this(PrincipalStub.pidFromPublicKey(publicKey), cn, serverJvm, subjectSetTokens, "")
+
+  def this(publicKey: PublicKey, cn: String, serverJvm: String, subjectSetTokens: String) {
+    this(PrincipalStub.pidFromPublicKey(publicKey), cn, serverJvm, Seq(subjectSetTokens), "")
+  }
+
+  def this(publicKey: PublicKey, cn: String, serverJvm: String, credTokens: Seq[String]) {
+    this(PrincipalStub.pidFromPublicKey(publicKey), cn, serverJvm, credTokens, "")
   }
 
   def this(publicKey: PublicKey, cn: String, serverJvm: String) {
     this(publicKey, cn, serverJvm, Seq[String]())
   }
  
-  def this(publicKey: PublicKey, serverJvm: String, subjectSetTokens: Seq[String]) {
-    this(PrincipalStub.pidFromPublicKey(publicKey), "", serverJvm, subjectSetTokens, "")
+  def this(publicKey: PublicKey, serverJvm: String, credTokens: Seq[String]) {
+    this(PrincipalStub.pidFromPublicKey(publicKey), "", serverJvm, credTokens, "")
   }
 
   def this(publicKey: PublicKey, serverJvm: String) {
     this(publicKey, "", serverJvm, Seq[String]())
   }
  
-  def this(keyFile: String, cn: String, serverJvm: String, subjectSetTokens: Seq[String]) {
-    this(PrincipalStub.pidFromFile(keyFile), cn, serverJvm, subjectSetTokens, keyFile)
+  def this(keyFile: String, cn: String, serverJvm: String, credTokens: Seq[String]) {
+    this(PrincipalStub.pidFromFile(keyFile), cn, serverJvm, credTokens, keyFile)
   }
  
   def this(keyFile: String, cn: String, serverJvm: String) {
     this(keyFile, cn, serverJvm, Seq[String]())
   }
 
-  def this(keyFile: String, serverJvm: String, subjectSetTokens: Seq[String]) {
-    this(PrincipalStub.pidFromFile(keyFile), "", serverJvm, subjectSetTokens, keyFile)
+  def this(keyFile: String, serverJvm: String, credTokens: Seq[String]) {
+    this(PrincipalStub.pidFromFile(keyFile), "", serverJvm, credTokens, keyFile)
   }
  
   def this(keyFile: String, serverJvm: String) {
@@ -52,7 +56,8 @@ class PrincipalStub (
 
   /* DeveloperAPI */
   override def toString(): String = {
-    s"""|${pid}
+    s"""|Principal info: 
+        |${pid}
         |${canonicalName}
         |${serverJvm}
         |${subjectSetTokens}
@@ -62,8 +67,13 @@ class PrincipalStub (
   def getPid(): String = pid
   def getCN(): String = canonicalName
   def getJvm(): String = serverJvm
-  def getSubjectSetTokens: Seq[String] = subjectSetTokens
+  def getCredSetTokens: Seq[String] = credSetTokens
   def getKeyFile(): String = keyFile
+
+
+  /** 
+   * APIs to call local slang instance
+   */
 
   def queryLocalSlang(inference: Safelang, goal: String, args: Seq[Constant]): Seq[Seq[Statement]] = {
     val query = Query(Seq(Structure(goal, args)))
@@ -77,39 +87,47 @@ class PrincipalStub (
     token
   } 
 
+
+  /** APIs for remote slang call (defcall) */
+
   /**
-   * Make a simple remote call. The remote call is simple as it follows 
-   * a simple convention:
-   * entryPoint(?ServerJVM, ?ServerPrincipal, ?Envs, ... ?Arg ...)
+   * @return settoken 
+   *  
+   * Make a simple remote call to host server. The remote call is simple as the call follows 
+   * a simple convention (see below) and expects a token returned from the server.
+   * entryPoint(?ServerJVM, ?ServerPrincipal, ?Envs, ?Args) 
    */
   def simpleRemoteCall(inference: Safelang, entryPoint: String, 
-      env: String = emptyEnvs, args: Seq[String] = Seq()): String = {
-    val queryArgs = (Seq(serverJvm, pid, env) ++ args).map(s => buildConstant(s))
+      envs: String = emptyEnvs, args: Seq[String] = Seq()): String = {
+    val queryArgs = (Seq(serverJvm, pid, envs) ++ args).map(s => buildConstant(s))
     val token: String = queryAndExtractToken(inference, entryPoint, queryArgs)
     token
   }
 
   /**
-   * Remote call to a specific server 
+   * Remote call to a target server, rather than the host server 
    * The caller must ensure that the principal is installed on the server
    */
   def remoteCallToServer(inference: Safelang, entryPoint: String, specifiedJvm: String, 
-      env: String = emptyEnvs, args: Seq[String] = Seq()): String = {
-    val queryArgs = (Seq(specifiedJvm, pid, env) ++ args).map(s => buildConstant(s))
+      envs: String = emptyEnvs, args: Seq[String] = Seq()): String = {
+    val queryArgs = (Seq(specifiedJvm, pid, envs) ++ args).map(s => buildConstant(s))
     val token: String = queryAndExtractToken(inference, entryPoint, queryArgs)
     token
   }
 
+
+  /** Common operations for SAFE principals */
+
   /**
-   * Format of the postIdSet defcall in local Slang:
-   * defcall postIdSet(?JVM, ?Principal, ?Envs, ?CN)
+   * Format of postRawIdSet defcall in local Slang:
+   * defcall postRawIdSet(?JVM, ?Principal, ?Envs, ?CN)
    */
   def postIdSet(inference: Safelang): String = {
-    simpleRemoteCall(inference, "postIdSet", args=Seq(canonicalName)) 
+    simpleRemoteCall(inference, "postRawIdSet", args=Seq(canonicalName)) 
   }
 
   /**
-   * Format of the postSubjectSet defcall in local Slang: 
+   * Format of postSubjectSet defcall in local Slang: 
    * defcall postSubjectSet(?JVM, ?Principal, ?Envs)
    */
   def postSubjectSet(inference: Safelang): String = {
@@ -117,23 +135,23 @@ class PrincipalStub (
   }
 
   /**  
-   * Post subject set and store the token into subjectSetTokens
-   * We assume each principal has only one subject set. So each
-   * principal only call this method once.
+   * Post subject set and store the token into credSetTokens
+   * A principal could have multiple cred sets. Tokens are
+   * stored in order when cred sets are created.
    */ 
-  def postSubjectSetAndGetToken(inference: Safelang): Unit = {
+  def postSubjectSetAndPlaceToken(inference: Safelang): Unit = {
     val token = postSubjectSet(inference)
-    if(!subjectSetTokens.contains(token)) { 
-    // Add the token into the subject token set if it hasn't been added yet
-      subjectSetTokens = subjectSetTokens :+ token
+    if(!credSetTokens.contains(token)) { 
+      // Add the token into the subject token set if it hasn't been added yet
+      credSetTokens = credSetTokens :+ token
     }
   }
 
   /**
-   * Update subject set with a link 
+   * Update subject set with a token
    */
-  def updateSubjectSet(inference: Safelang, link: String): String = {
-    simpleRemoteCall(inference, "updateSubjectSet", args=Seq(link))
+  def updateSubjectSet(inference: Safelang, token: String): String = {
+    simpleRemoteCall(inference, "updateSubjectSet", args=Seq(token))
   } 
 }
 
