0f295de879faef0bcccea68c535ae4040c683d1a
qiangcao <caoqiang.cs@gmail.com>
2019-07-17 12:29:01 -0400

    continued work on checking rules for property lists

--- safe-styla/progs/plist.pl
+++ safe-styla/progs/plist.pl
@@ -1,21 +1,51 @@
 % Designed to operate on sorted lists.
+% Set libraryOn in application conf when launching styla shell.
+% TODO: dealing with non-numerical key values.
 
 % Is L a subsequence of R.
 subseq([X|L], [X|R]) :-
    subseq(L, R).
-subseq(L, [_|R]) :-
-   subseq(L, R).
-subseq([], []).
 
-% Does each property K in L have one corresponding [K,V] pair in R.
-propsPresent([], []).
-propsPresent([K|L], [[?K,_]|R]):-      
+subseq([H|L], [S|R]) :-
+   S < H,
+   subseq([H|L], R).
+%subseq(L, [S|R]) :-
+%   [H|_] = L,
+%   traceln(H),
+%   S < H,
+%   subseq(L, R).
+
+subseq([], R).
+%subseq(L, R) :- L = [].
+%subseq(L, R) :- eq(L, []).
+
+% Queries:
+% subseq([1,3], [0,1,2,3,4,5]).
+% subseq(["alice", "bob", "cindy"], ["alice", "bob", "cindy", "david", "frank", "george"]).
+
+% Is for each property K in L there one corresponding [K,V] pair in R.
+propsPresent([], R).
+propsPresent([K|L], [[K,_]|R]):-      
   propsPresent(L, R).
-propsPresent(L, [_|R]):-
+propsPresent(L, [[S,_]|R]):-
+  [H|_] = L,
+  S < H,
   propsPresent(L, R).
+
+% Queries
+% propsPresent([1,3,4], [[1, "alice"], [2, "bob"], [3, "cindy"], [4, "david"], [5, "frank"]).
+% propsPresent(["port", "protocol"], [["master_addr", "152.3.136.112"], ["port", 10009], ["protocol", "http"]]). 
 
 % Does any property K in L have a corresponding [K,V] pair in R.
 anyPropPresent([K|_],[[K,_]|_]).
-anyPropPresent([_|L],R) :-
+anyPropPresent(L, [[S,_]|R]) :-
+  [H|_] = L,
+  S < H,
+  anyPropPresent(L,R).
+anyPropPresent([S|L], R) :-
+  [[H,_]|_] = R,
+  S < H,
   anyPropPresent(L,R).
 
+% Queries
+% anyPropPresent([1, 2], [[2, "bob"], [3, "cindy"]]).
