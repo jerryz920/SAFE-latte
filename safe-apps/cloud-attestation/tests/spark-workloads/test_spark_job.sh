23a440474f950f4558fb02604bbd2051a90897b6
qiangcao <caoqiang.cs@gmail.com>
2020-07-19 23:56:53 -0400

    Checking the checker: checking configset of configset

--- safe-apps/cloud-attestation/tests/spark-workloads/test_spark_job.sh
+++ safe-apps/cloud-attestation/tests/spark-workloads/test_spark_job.sh
@@ -24,7 +24,7 @@ postSparkPod "2df11174-0b17-4056-a8e7-f248876f7acf" "worker"
 postSparkPod "776740af-4895-4eaf-83a3-b7552227b13b" "worker"
 postSparkPod "8d827ee8-91a6-4402-9df5-ed2414d6edcd" "worker"
  
-# Spark-worker admission policy
+# ConfigSet for worker admission
 postPodPolicy gabbi confexec safe-spark-worker
 postImagePolicy gabbi confexec '[\"spark:v2.3\",image_c2,image_c3]'
 postProhibitedPolicy gabbi confexec '[\"spark:v2.3\", \"KUBERNETES_PROHIBITED_KEY3\"]'
@@ -54,7 +54,7 @@ checkPodAttestation gabbi $kmaster "8d827ee8-91a6-4402-9df5-ed2414d6edcd" confex
 
 # ConfigSet by another principal                                                                              
 postPodPolicy jack jconfexec safe-spark-worker
-postImagePolicy jack jconfexec '[\"spark:v2.3\",image_00,image_01]'
+postImagePolicy jack jconfexec '[image_00,image_01,\"spark:v2.3\"]'
 postProhibitedPolicy jack jconfexec '[\"spark:v2.3\", \"KUBERNETES_PROHIBITED_KEY1\"]'
 postProhibitedPolicy jack jconfexec '[\"spark:v2.3\", \"KUBERNETES_PROHIBITED_KEY2\"]'
 postRequiredPolicy jack jconfexec '[\"spark:v2.3\",\"KUBERNETES_SERVICE_PORT\"]'
@@ -83,9 +83,9 @@ checkWorker ${driver} $kmaster "776740af-4895-4eaf-83a3-b7552227b13b" ${drivergr
 checkWorker ${driver} $kmaster "8d827ee8-91a6-4402-9df5-ed2414d6edcd" ${drivergroup}
 
 
-# Driver policy  
+# Driver ConfigSet  
 postPodPolicy bob configd safe-spark-job
-postImagePolicy bob configd '[\"spark:v2.3\",\"spark:v2.3\"]'
+postImagePolicy bob configd '[image_00, image_01, \"spark:v2.3\",\"spark:v2.3\"]'
 postProhibitedPolicy bob configd '[\"spark:v2.3\", \"KUBERNETES_PROHIBITED_KEY1\"]' 
 postProhibitedPolicy bob configd '[\"spark:v2.3\", \"KUBERNETES_PROHIBITED_KEY2\"]' 
 postRequiredPolicy bob configd '[\"spark:v2.3\",\"KUBERNETES_SERVICE_PORT\",\"SPARK_DRIVER_ARGS\"]'
@@ -115,3 +115,39 @@ checkPodAccess frank $kmaster "026f48d4-3a68-42fd-b8b0-9c94f00b1f1a" alice:tag0
 checkPodAccess frank $kmaster "2df11174-0b17-4056-a8e7-f248876f7acf" alice:tag0
 checkPodAccess frank $kmaster "776740af-4895-4eaf-83a3-b7552227b13b" alice:tag0
 checkPodAccess frank $kmaster "8d827ee8-91a6-4402-9df5-ed2414d6edcd" alice:tag0
+
+
+# Check the checker: ConfigSet of Driver ConfigSet
+postPodPolicy tom driveradm safe-drivergroup
+postImagePolicy tom driveradm '[\"image_00\", \"image_01\", \"image_02\", \"spark:v2.3\", \"spark:v2.3\"]'
+
+postRequiredPolicy tom driveradm '[[\"spark:v2.3\", prohibited], \"KUBERNETES_PROHIBITED_KEY1\"]'
+postProhibitedPolicy tom driveradm '[[\"spark:v2.3\", prohibited], \"KUBERNETES_PROHIBITED_PROHIBITED_KEY1\"]'
+postQualifierPolicy tom driveradm '[[\"spark:v2.3\", prohibited]]'
+
+postRequiredPolicy tom driveradm '[[\"spark:v2.3\", prohibited], \"KUBERNETES_PROHIBITED_KEY2\"]'
+postProhibitedPolicy tom driveradm '[[\"spark:v2.3\", prohibited], \"KUBERNETES_PROHIBITED_PROHIBITED_KEY2\"]'
+postQualifierPolicy tom driveradm '[[\"spark:v2.3\", prohibited]]'
+
+postRequiredPolicy tom driveradm '[[\"spark:v2.3\", required], \"KUBERNETES_SERVICE_PORT\"]'
+postProhibitedPolicy tom driveradm '[[\"spark:v2.3\", required], \"KUBERNETES_OPTION_NOT_EXIST_0\"]'
+postQualifierPolicy tom driveradm '[[\"spark:v2.3\", required]]'
+
+postRequiredPolicy tom driveradm '[[\"spark:v2.3\", required], \"SPARK_EXECUTOR_ID\"]'
+postProhibitedPolicy tom driveradm '[[\"spark:v2.3\", required], \"KUBERNETES_OPTION_NOT_EXIST_1\"]'
+postQualifierPolicy tom driveradm '[[\"spark:v2.3\", required]]'
+
+postRequiredPolicy tom driveradm '[[\"spark:v2.3\", qualifier], \"arg0\"]'
+postProhibitedPolicy tom driveradm '[[\"spark:v2.3\", qualifier], \"SPARK_OPTION_NOT_EXIST_2\"]'
+postQualifierPolicy tom driveradm '[[\"spark:v2.3\", qualifier], [\"SPARK_DRIVER_URL\",\"spark://CoarseGrainedScheduler@spatial-spark-26cc9625ccb0362f834ba50405af6879-driver-svc.latte-gabbi.svc:7078\"]]'
+
+postRequiredPolicy tom driveradm '[[\"spark:v2.3\", qualifier], \"arg0\", \"arg1\"]'
+postProhibitedPolicy tom driveradm '[[\"spark:v2.3\", qualifier], \"SPARK_OPTION_NOT_EXIST_3\"]'
+postQualifierPolicy tom driveradm '[[\"spark:v2.3\", qualifier], [\"arg0\",\"init\"]]'
+
+postEndorsementLink tom trustPolicy/diveradm
+
+printf "\n\nCheck driver group\n\n"
+
+# This driver-group check should pass
+checkDriverGroup tom $kmaster "026f48d4-3a68-42fd-b8b0-9c94f00b1f1a" gabbi driveradm
